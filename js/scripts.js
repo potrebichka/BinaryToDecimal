723a4a5f5df50167b048d9b4e23c599400c0f2c3
Nina Potrebich <potrebich@gmail.com>
2019-10-30 23:32:11 -0700

    add project converting binary number to decimsl

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
+    $("#form1").submit(function(event) {
+        event.preventDefault();
+        var binary = $("#number").val();
+        //var binary = binaryStr.split("").reverse().join("");
+        var decimal = 0;
+        var power = 0;
+        for (var i = binary.length-1; i>=0; i--) {
+            console.log(power);
+            if (parseInt(binary[i]) === 1) {
+                decimal += Math.pow(2, power);
+            }
+            power+=1;
+        }
 
+        $("#binary").text(binary);
+        $("#decimal").text(decimal);
+        $("#resultBox").show();
+    })
 
 
 });
